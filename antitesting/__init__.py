30e3a6dc36fd69c236696a4b4759c52e852ef746
Evgeniy Blinov <zheni-b@yandex.ru>
2023-12-27 14:45:08 +0300

    proxy module

--- antitesting/__init__.py
+++ antitesting/__init__.py
@@ -1,1 +1,7 @@
-from antitesting.add import add_forbidding_file  # noqa: F401
+import sys
+
+from antitesting.proxy_module import ProxyModule
+from antitesting.hook import pytest_collection_modifyitems
+
+
+sys.modules[__name__].__class__ = ProxyModule
