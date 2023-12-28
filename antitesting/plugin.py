a1c4efcc361987728dd6f941ec724b6993c07a1a
Evgeniy Blinov <zheni-b@yandex.ru>
2023-12-28 18:31:23 +0300

    type hint

--- antitesting/plugin.py
+++ antitesting/plugin.py
@@ -6,7 +6,7 @@ from antitesting.state import collector
 from antitesting.protocols.item import ItemProtocol
 
 
-@hookimpl
+@hookimpl  # type: ignore[misc]
 def pytest_collection_modifyitems(session: Session, config: Config, items: List[ItemProtocol]) -> None:
     forbidden_ids = [index for index, item in enumerate(items) if item in collector]
     forbidden_ids.reverse()
