a852ff65c75c7e7342282ed3ba20d905ca3908c7
Evgeniy Blinov <zheni-b@yandex.ru>
2023-12-28 18:23:58 +0300

    lint

--- antitesting/protocols/item.py
+++ antitesting/protocols/item.py
@@ -3,7 +3,7 @@ from typing import Any
 try:
     from typing import Protocol, runtime_checkable
 except ImportError:  # pragma: no cover
-    from typing_extensions import Protocol, runtime_checkable
+    from typing_extensions import Protocol, runtime_checkable  # type: ignore[assignment]
 
 
 @runtime_checkable
