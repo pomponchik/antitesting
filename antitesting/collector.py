cff3e306dcb12ef0639fbe9a69265d9cb2553a48
Evgeniy Blinov <zheni-b@yandex.ru>
2024-01-02 15:48:30 +0300

    +1 empty line

--- antitesting/collector.py
+++ antitesting/collector.py
@@ -44,6 +44,7 @@ class DisabledTestsCollector:
 
     def check_unique_test_names(self, items: List[ItemProtocol]) -> None:
         unique_items_names = {item.name for item in items}
+        
         for collected_test in self:
             if collected_test.name not in unique_items_names:
                 raise UndefinedTestNameError(f'There is no test named "{collected_test.name}". You specified this name in the skip list.')
